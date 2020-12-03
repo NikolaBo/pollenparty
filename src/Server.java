1438381641421d8284275e238acb244946f01f65
nikolabo <nikolabo@live.com>
2020-12-03 13:01:44 -0800

    begin implementing share page

--- src/Server.java
+++ src/Server.java
@@ -17,6 +17,10 @@ public class Server {
             String html = Files.readString(Paths.get("index.html"));
             send(t, "text/html; charset=utf-8", html);
         });
+        server.createContext("/share", (HttpExchange t) -> {
+            String html = Files.readString(Paths.get("share.html"));
+            send(t, "text/html; charset=utf-8", html);
+        });
         server.createContext("/api/geocode", (HttpExchange t) -> {
             String zip = parse("zip", t.getRequestURI().getQuery().split("&"));
             System.out.println("ZIP received: " + zip);
